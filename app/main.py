63fe08f174d85b71d7273d1c2fa4c280dff673a0
Rajvel437 <rajvelrajvel4@gmail.com>
2026-01-21 23:10:51 +0530

    Add API v1 init and update main app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,40 @@
 from fastapi import FastAPI
-from app.routes.api.v1.endpoints import user
+from contextlib import asynccontextmanager
+import logging
+
+# from app.core.config import settings
+# from app.core.logging import setup_logging
+# from app.api.v1.router import api_router
+# from app.db.session import init_db, close_db
+# from app.observability.metrics import init_metrics
+# from app.middlewares import register_middlewares
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # ---------- STARTUP ----------
+    logging.info("aplication started")
 
-app.include_router(user.router, prefix="/api/v1/users", tags=["users"])
+    yield  # app is running
+
+    # ---------- SHUTDOWN ----------
+    # await close_db()
+    logging.info("Application shut down")
 
 
-@app.get("/")
-async def home():
-    return {"msg":"App is running successfully"}
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Ecomerce backend",
+        version="1.0.0",
+        lifespan=lifespan,
+        docs_url="/docs",
+        redoc_url="/redoc",
+    )
+
+    register_middlewares(app)
+    app.include_router(api_router, prefix="/api")
+
+    return app
 
 
+app = create_app()
